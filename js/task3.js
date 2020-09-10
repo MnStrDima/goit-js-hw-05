5103ebeb95f2e9a32ee7d39c9e184be0d7f79d83
Dmitry Medychenko <rAv3ndima91@gmail.com>
2020-09-10 21:39:40 +0300

    решает дз №5

--- js/task3.js
+++ js/task3.js
@@ -11,6 +11,21 @@
 
 // Write code under this line
 
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(item) {
+    this.items.splice(this.items.indexOf(item), 1);
+  }
+}
+
 console.log(typeof Storage);
 // 'function'
 
